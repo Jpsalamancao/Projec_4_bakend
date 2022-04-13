d45c907706eb9e8df31f87b38cc0ec39d7a384e2
unknown <jpsalamancao@gmail.com>
2022-04-12 22:17:03 -0500

    start bakend

---
+++ index.js
@@ -0,0 +1,14 @@
+const factory = ('./src/factory/factory.js');
+const validator = ('./src/utils/validateHeaders.js');
+
+
+
+
+// var http= require('http');
+// http.createServer(function(peticion, respuesta){
+//     respuesta.writeHead(200, {'content-type':'text/html'});
+//     respuesta.write('<h3> hola mundo </h3>')
+//     respuesta.end();
+// });
+
+// servidor.listen(3000);
